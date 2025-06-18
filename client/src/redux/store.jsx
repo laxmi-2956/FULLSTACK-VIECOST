07d96cb22a3b62243e02380588f9105e2206cf5c
laxmi-2956 <156445712+laxmi-2956@users.noreply.github.com>
2025-06-18 20:45:19 +0530

    Update store.jsx

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import authReducer from "./auth/authslice"
-import authReducer from "./auth/authSlice";
+
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
+import { authslice } from "./auth/authslice";
 
 const persistConfig = {
   key: "root",
   storage,
 };
-const rootReducer = combineReducers({ auth: authReducer});
+const rootReducer = combineReducers({ auth : authslice.reducer });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
