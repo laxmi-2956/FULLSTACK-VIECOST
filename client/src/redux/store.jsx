ea6fca8b1a4df97a8b8b3b7b753696c7f5860cb8
laxmi-2956 <156445712+laxmi-2956@users.noreply.github.com>
2025-06-18 16:03:33 +0530

    Add files via upload

---
+++ client/src/redux/store.jsx
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./auth/authslice"
+import authReducer from "./auth/authSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({ auth: authReducer});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
